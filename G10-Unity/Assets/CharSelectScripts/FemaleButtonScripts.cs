bce69a0a333579eb42fa523a4484b491b86f8705
YukiSato <yukiyukiamam@gmail.com>
2019-05-10 20:43:11 +0900

    シーンの遷移の実装

---
+++ G10-Unity/Assets/CharSelectScripts/FemaleButtonScripts.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FemaleButtonScripts : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+    {
+        print("yyyyyy");
+	}
+
+    private void OnMouseOver()
+    {
+        print("sdfsdfsdfsd");
+    }
+
+    private void OnMouseEnter()
+    {
+        print("kkkkkk");
+    }
+
+    // Update is called once per frame
+    void Update () 
+    {
+		
+	}
+}
