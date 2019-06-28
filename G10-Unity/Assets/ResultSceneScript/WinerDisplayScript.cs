35f15e573e1da02e56edfe20042240945f855ab5
Ryoma Iguchi <ryomaigu531@icloud.com>
2019-06-28 15:22:31 +0900

    結果画面の勝利プレイヤーの表示編集

---
+++ G10-Unity/Assets/ResultSceneScript/WinerDisplayScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class WinerDisplayScript : MonoBehaviour
+{
+
+    Text Winer;
+
+    // Use this for initialization
+    void Start()
+    {
+        Winer = GetComponentInChildren<Text>();//UIのテキストの取得の仕方
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            print("A");
+            Winer.text = "~1P~";
+        }
+        if (Input.GetKeyDown("space"))
+        {
+            print("space");
+            Winer.text = "~2P~";
+        }
+    }
+}
