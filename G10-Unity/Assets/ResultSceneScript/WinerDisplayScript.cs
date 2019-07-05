430ae74c4cb5261e34700aafa8cde9a84bd09925
Ryoma Iguchi <ryomaigu531@icloud.com>
2019-07-05 13:36:02 +0900

    音楽の追加

--- G10-Unity/Assets/ResultSceneScript/WinerDisplayScript.cs
+++ G10-Unity/Assets/ResultSceneScript/WinerDisplayScript.cs
@@ -19,12 +19,12 @@ public class WinerDisplayScript : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.A))
         {
             print("A");
-            Winer.text = "~1P~";
+            Winer.text = "~1P~";//キーボードのAを押すと1Pと表示される
         }
         if (Input.GetKeyDown("space"))
         {
             print("space");
-            Winer.text = "~2P~";
+            Winer.text = "~2P~";//キーボードのspaceを押すと2Pと表示される
         }
     }
 }
